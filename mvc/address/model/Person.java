af8912a85425f91b234d83a42b146f4ac7e43118
Juanfro <juanfropro@gmail.com>
2019-01-18 13:18:03 +0100

    Primera ejecución. Person

---
+++ mvc/address/model/Person.java
@@ -0,0 +1,15 @@
+package address.model;
+
+/**
+ * Model class for a person
+ * 
+ * @author Juan Antonio Rodriguez
+ *
+ */
+public class Person {
+
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
