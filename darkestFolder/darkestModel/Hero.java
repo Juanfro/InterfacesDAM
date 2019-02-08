8d7df5188ba2b9e2cadef2e085e1cf68b2464f79
Juanfro <juanfropro@gmail.com>
2019-02-08 13:26:37 +0100

    Clase 8-2

--- darkestFolder/darkestModel/Hero.java
+++ darkestFolder/darkestModel/Hero.java
@@ -1,6 +1,12 @@
 package darkestModel;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.ObservableList;
 
@@ -13,8 +19,15 @@ public class Hero {
 
 	private StringProperty name;
 	private Type type;
+	private IntegerProperty level;
 	private IntegerProperty armorValue;
 	private IntegerProperty weaponValue;
+	private ListProperty<String> trinkets;
+	private BooleanProperty survived;
+	private BooleanProperty isAlive;
+	private ObjectProperty<LocalDate> deathDate;
+	private Location deathLocation;
+	private ObjectProperty<Location> deathLocation2;
 	// private List
 
 }
---
+++ darkestFolder/darkestModel/Location.java
@@ -0,0 +1,5 @@
+package darkestModel;
+
+public class Location {
+
+}
