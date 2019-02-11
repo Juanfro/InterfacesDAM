6277f252f53684956cc0403ab07df0a84e813530
Juanfro <juanfropro@gmail.com>
2019-02-11 11:21:54 +0100

    Clase 11-2

--- darkestFolder/darkestModel/Hero.java
+++ darkestFolder/darkestModel/Hero.java
@@ -12,22 +12,90 @@ import javafx.collections.ObservableList;
 
 public class Hero {
 
-	private enum Type {
+	public enum HeroType {
 		ABOMINATION, ANTIQUARIAN, ARBALEST, BOUNTY_HUNTER, CRUSADER, GRAVE_ROBBER_HELLION, HIGHWAYMAN, HOUNDMASTER,
 		JESTER, LEPER, MAN_AT_ARMS, OCCULTIST, PLAGUE_DOCTOR, VESTAL
 	}
 
 	private StringProperty name;
-	private Type type;
+	private HeroType type;
 	private IntegerProperty level;
-	private IntegerProperty armorValue;
-	private IntegerProperty weaponValue;
-	private ListProperty<String> trinkets;
+	private IntegerProperty armorTier;
+	private IntegerProperty weaponTier;
+	// private ListProperty<String> trinkets;
 	private BooleanProperty survived;
 	private BooleanProperty isAlive;
-	private ObjectProperty<LocalDate> deathDate;
-	private Location deathLocation;
-	private ObjectProperty<Location> deathLocation2;
+
+	// private ObjectProperty<LocalDate> deathDate;
+	// private Location deathLocation;
+	// private ObjectProperty<Location> deathLocation2;
 	// private List
+
+	protected Hero(StringProperty name, HeroType type, IntegerProperty level, IntegerProperty armorTier,
+			IntegerProperty weaponTier, BooleanProperty survived, BooleanProperty isAlive) {
+		this.name = name;
+		this.type = type;
+		this.level = level;
+		this.armorTier = armorTier;
+		this.weaponTier = weaponTier;
+		this.survived = survived;
+		this.isAlive = isAlive;
+	}
+
+	protected StringProperty getName() {
+		return name;
+	}
+
+	protected void setName(StringProperty name) {
+		this.name = name;
+	}
+
+	protected HeroType getType() {
+		return type;
+	}
+
+	protected void setType(HeroType type) {
+		this.type = type;
+	}
+
+	protected IntegerProperty getLevel() {
+		return level;
+	}
+
+	protected void setLevel(IntegerProperty level) {
+		this.level = level;
+	}
+
+	protected IntegerProperty getArmorTier() {
+		return armorTier;
+	}
+
+	protected void setArmorTier(IntegerProperty armorTier) {
+		this.armorTier = armorTier;
+	}
+
+	protected IntegerProperty getWeaponTier() {
+		return weaponTier;
+	}
+
+	protected void setWeaponTier(IntegerProperty weaponTier) {
+		this.weaponTier = weaponTier;
+	}
+
+	protected BooleanProperty getSurvived() {
+		return survived;
+	}
+
+	protected void setSurvived(BooleanProperty survived) {
+		this.survived = survived;
+	}
+
+	protected BooleanProperty getIsAlive() {
+		return isAlive;
+	}
+
+	protected void setIsAlive(BooleanProperty isAlive) {
+		this.isAlive = isAlive;
+	}
 
 }
--- darkestFolder/darkestPackage/DarkestProject.java
+++ darkestFolder/darkestPackage/DarkestProject.java
@@ -1,19 +1,33 @@
 package darkestPackage;
 
+import darkestModel.Hero;
+import darkestModel.Hero.HeroType;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class DarkestProject extends Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
+	private ObservableList<Hero> heroData = FXCollections.observableArrayList();
 
+	public DarkestProject() {
+		//heroData.add(new He)
 	}
-
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 
 	}
 
+	public static void main(String[] args) {
+		
+		launch(args);
+		
+	}
 }
